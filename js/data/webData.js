d0d8616568415ac0fbfd2b55f2bb88ff3057632a
WaffleAWT <122714317+WaffleAWT@users.noreply.github.com>
2025-02-27 15:55:30 +0300

    Fixed website data paths

--- js/data/webData.js
+++ js/data/webData.js
@@ -12,7 +12,7 @@ const webData = {
             title: "Herais Healthcare",
             image: "Assets/Healthcare.png",
             description: "HERAIS Health Care is a leading Canadian supplier of healthcare products, rehabilitation equipment, and medical consumables, backed by over 15 years of industry expertise. An ISO & SEFA certified group. FDA & CE approved.",
-            url: "#"
+            url: "https://heraishealthcare.com"
         }
     ]
 };
